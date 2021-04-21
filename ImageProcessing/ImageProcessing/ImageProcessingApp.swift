76d6943f59b550c9e3d1f90ccff76dac90ebc4a8
StuTan <stutan@foxmail.com>
2021-04-21 09:07:28 +0800

    application created

---
+++ ImageProcessing/ImageProcessing/ImageProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProcessingApp.swift
+//  ImageProcessing
+//
+//  Created by StuTan on 2021/4/19.
+//
+
+import SwiftUI
+
+@main
+struct ImageProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
